eed0c901f032f67a4895658b58387635116624bd
meridian34 <sobol.andrey.25@gmail.com>
2021-03-09 19:33:11 +0200

    Добавьте файлы проекта.

---
+++ Modul_2_Practice_3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Modul_2_Practice_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
